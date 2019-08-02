10191633dc46920e75d2daab92be8bcf569bf3a3
lamiru <lamiru@gmail.com>
2019-08-02 19:46:33 +0900

    add redirectView

--- src/main/java/net/url/controller/UrlMapperController.java
+++ src/main/java/net/url/controller/UrlMapperController.java
@@ -1,11 +1,19 @@
 package net.url.controller;
 
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 import net.url.domain.UrlMapper;
 import net.url.repository.UrlMapperRepository;
@@ -34,19 +42,27 @@ public class UrlMapperController {
 	    return shortenedUrl.toString();
 	}
 	
-	@PostMapping("/create")
+	@PostMapping("/m/create")
 	public String create(UrlMapper url_mapper) {
 		url_mapper = urlMapperRepository.save(url_mapper);
 		String shortenedUrl = idToShortenedUrl(url_mapper.getId());
 		url_mapper.setShortenedUrl(shortenedUrl);
 		urlMapperRepository.save(url_mapper);
-		return "redirect:/list";
+		return "redirect:/m/list";
 	}
 	
-	@GetMapping("/list")
+	@GetMapping("/m/list")
 	public String list(Model model) {
 		model.addAttribute("urlMappers", urlMapperRepository.findAllByOrderByIdDesc());
 		return "list";
 	}
+
+	@GetMapping("/{shortenedUrl}")
+    public RedirectView redirectUrl(@PathVariable String shortenedUrl) {
+		UrlMapper url_mapper = urlMapperRepository.findByShortenedUrl(shortenedUrl);
+        RedirectView redirectView = new RedirectView();
+        redirectView.setUrl(url_mapper.getOriginalUrl());
+        return redirectView;
+    }
 	
 }
--- src/main/java/net/url/repository/UrlMapperRepository.java
+++ src/main/java/net/url/repository/UrlMapperRepository.java
@@ -2,13 +2,15 @@ package net.url.repository;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import net.url.domain.UrlMapper;
 
 
-public interface UrlMapperRepository extends JpaRepository<UrlMapper, Long> {
+public interface UrlMapperRepository extends CrudRepository<UrlMapper, Long> {
 	public List<UrlMapper> findAllByOrderByIdAsc();
 	
 	public List<UrlMapper> findAllByOrderByIdDesc();
+	
+	public UrlMapper findByShortenedUrl(String shortenedUrl);
 }
